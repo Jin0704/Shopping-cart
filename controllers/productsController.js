8a6cf6988a334c217edb06594709055f739a5b06
Jin <s87160204@gmail.com>
2021-10-26 00:03:16 +0800

    Refactor: Funciton in  productsController to async function

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,59 +1,46 @@
 const db = require('../models')
 const Product = db.Product
 const Cart = db.Cart
-const PAGE_LIMIT = 3
+const PAGE_LIMIT = 6
 const PAGE_OFFSET = 0
 
 
 let ProductController = {
-  getProducts: (req, res) => {
-    Product.findAndCountAll({
+  getProducts: async (req, res) => {
+    const products = await Product.findAndCountAll({
       raw: true,
       nest: true,
       offset: PAGE_OFFSET,
       limit: PAGE_LIMIT
-    }).then(products => {
-      //sidebar page
-      return Cart.findByPk(req.session.cartId, {
-        include: 'items'
-      }).then(cart => {
-        cart = cart ? cart.toJSON() : { items: [] }
-        let totalPrice = cart.items.length > 0 ? cart.items.map(d => d.price * d.CartItem.quantity).reduce((a, b) => a + b) : 0
-        // console.log(products)
-        return res.render('products', {
-          products,
-          cart,
-          totalPrice
-        })
-      })
+    })
+
+    let cart = await Cart.findByPk(req.session.cartId, {
+      include: 'items'
+    })
+    //sidebar page
+    cart = cart ? cart.toJSON() : { items: [] }
+    let totalPrice = cart.items.length > 0 ? cart.items.map(d => d.price * d.CartItem.quantity).reduce((a, b) => a + b) : 0
+
+    return res.render('products', {
+      products,
+      cart,
+      totalPrice
     })
   },
-  // getProduct: (req, res) => {
-  //   Product.findByPk(req.params.id)
-  //     .then(product => {
-  //       return res.render('product', { product: product.toJSON() })
-  //     })
-  // },
-  getProduct: (req, res) => {
-    Product.findByPk(req.params.id)
-      .then(product => {
-        //sidebar page
-        return Cart.findByPk(req.session.cartId, { include: 'items' })
-          .then(cart => {
-            cart = cart ? cart.toJSON() : { items: [] }
-            let totalPrice = cart.items.length > 0 ? cart.items.map(d => d.price * d.CartItem.quantity).reduce((a, b) => a + b) : 0
 
-            return res.render('product', {
-              product: product.toJSON(),
-              cart: cart,
-              totalPrice: totalPrice
-            })
-          })
-      })
+  getProduct: async (req, res) => {
+    const product = await Product.findByPk(req.params.id)
+    let cart = await Cart.findByPk(req.session.cartId, { include: 'items' })
+    //sidebar page
+    cart = cart ? cart.toJSON() : { items: [] }
+    let totalPrice = cart.items.length > 0 ? cart.items.map(d => d.price * d.CartItem.quantity).reduce((a, b) => a + b) : 0
+
+    return res.render('product', {
+      product: product.toJSON(),
+      cart: cart,
+      totalPrice: totalPrice
+    })
   }
-
 }
-
-
 
 module.exports = ProductController

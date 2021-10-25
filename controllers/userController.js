c6cf907caf6f69ff119c29521020dfd7dbf76be2
Jin <s87160204@gmail.com>
2021-10-25 23:39:05 +0800

    Refactor: userController to async funciton

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,27 +12,25 @@ const userController = {
     return res.render('signup')
   },
 
-  SignUp: (req, res) => {
+  SignUp: async (req, res) => {
     // console.log(req.body)
     if (req.body.confirmpassword !== req.body.password) {
       req.flash('error_messages', '兩次密碼輸入不同')
       return res.redirect('/signup')
-    } else {
-      User.findOne({ where: { email: req.body.email } }).then(user => {
-        if (user) {
-          req.flash('error_messages', '信箱重複')
-          return res.redirect('/signup')
-        } else {
-          User.create({
-            email: req.body.email,
-            password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(6))
-          }).then(user => {
-            req.flash('success_messages', '註冊成功')
-            return res.redirect('/signin')
-          })
-        }
-      })
     }
+    const user = await User.findOne({ where: { email: req.body.email } })
+    if (user) {
+      req.flash('error_messages', '信箱重複')
+      return res.redirect('/signup')
+    }
+
+    await User.create({
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(6))
+    })
+
+    req.flash('success_messages', '註冊成功')
+    return res.redirect('/signin')
   },
 
   Signin: (req, res) => {

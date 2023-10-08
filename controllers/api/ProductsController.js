e288bb9decbf64f817d0f3beb942f417fb7468b2
Jin0704 <s87160204@gmail.com>
2023-10-09 00:06:40 +0800

    perf:product api

--- controllers/api/ProductsController.js
+++ controllers/api/ProductsController.js
@@ -1,24 +1,24 @@
 const ProductService = require('../../services/product')
 const responseBuilder = require('../../helper/responseBuilder')
-
+const redis = require('../../redis')
 let ProductController = {
 
   // 要再將search跟sort 合併進這個controller
   getProducts: async (req, res) => {
-    let data;
     try {
-      data = await ProductService.getProducts(req)
+      let data = await redis.getKey('api-products') ? await redis.getKey('api-products'):await ProductService.getProducts(req)
+      return responseBuilder.success(res, 200, data)
     } catch (err) {
       console.error(err)
       return responseBuilder.error(req, res, 400, err)
     }
-    return responseBuilder.success(res, 200, data)
+
   },
 
   getProduct: async (req, res) => {
     let data;
     try {
-      data = await ProductService.getProduct(req)
+      let data = await redis.getKey(`api-products-${req.params.id}`) ? await redis.getKey(`api-products-${req.params.id}`):await ProductService.getProduct(req)
     } catch (err) {
       console.error(err)
       return responseBuilder.error(req, res, 400, err)

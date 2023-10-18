807f7a58f0a22eb9947e8a7d78e708ae7a2f3d97
Jin0704 <s87160204@gmail.com>
2023-10-18 22:55:21 +0800

    feat:add convert function to computeHelper

--- helper/compute.js
+++ helper/compute.js
@@ -1,5 +1,6 @@
 const db = require('../models')
-
+const SettingService = require('../services/admin/setting')
+const settingService = new SettingService(db)
 //Todo : Refactor
 class ComputeHelper {
   static async compute(input){
@@ -24,6 +25,16 @@ class ComputeHelper {
       }else{
         return amount * promotionCode.discount / 100
       }
+    }catch(err){
+      console.error(err)
+      throw new Error(err)
+    }
+  }
+
+  static async convertOrderAmountToRewardPoint(amount){
+    try{
+      const settingCondition = await settingService.getSettings()
+      return (+amount/settingCondition.conversionAmountForRewardPoint).toFixed(2)
     }catch(err){
       console.error(err)
       throw new Error(err)
